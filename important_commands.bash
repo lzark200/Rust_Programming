cc4c4417d4d04f380d036dafb93152fab3515abd
harunoSak200 <kshatriyaprojectsgithubs@gmail.com>
2025-07-31 21:41:24 +0530

    rust basic installation and the basic commands done!

---
+++ important_commands.bash
@@ -0,0 +1,20 @@
+# pwd command : tells the current working directory in which we are working.
+command : pwd
+
+# ls command : list all the files and folder within the current working directory.
+command : ls
+
+# cd command : it is used to change the directory
+command : cd
+
+#command to check the rust version : 
+command : rustc --version 
+
+# rustup that helps us to update , install and uninstall the rust compiler from the computer.
+command : rustup  # this shows multiple functions rustup can perform we can chose from the options list.
+
+# rustup command to uinstall the rust : 
+command : rustup self uninstall
+
+# rustup command to open the rust official documentation : 
+command : rustup doc
